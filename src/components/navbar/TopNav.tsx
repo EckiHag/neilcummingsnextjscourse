edc434a2bed60aa886b8297d0fefe199c93b3162
Eckhard Hagemeier <74151940+EckiHag@users.noreply.github.com>
2024-08-08 20:30:44 +0200

    login register Formulare

--- src/components/navbar/TopNav.tsx
+++ src/components/navbar/TopNav.tsx
@@ -1,3 +1,62 @@
+// "use client"
+
+// import { useState } from "react"
+// import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react"
+// import Link from "next/link"
+// import { PiAxe } from "react-icons/pi"
+// import { HiOutlineMenu, HiX } from "react-icons/hi"
+// import NavLink from "./NavLink"
+
+// export default function TopNav() {
+//   const [menuOpen, setMenuOpen] = useState(false)
+
+//   return (
+//     <Navbar maxWidth="xl" className="bg-gradient-to-r from-green-400 to-green-700 relative">
+//       <NavbarBrand as={Link} href="/" className="flex items-center">
+//         <PiAxe size={40} className="text-gray-200 pr-2" />
+//         <div className="font-bold text-3xl flex">
+//           <span className="text-gray-900">Eckis</span>
+//           <span className="text-gray-200">App</span>
+//         </div>
+//       </NavbarBrand>
+//       <div className="md:hidden flex items-center">
+//         <button onClick={() => setMenuOpen(!menuOpen)}>{menuOpen ? <HiX size={30} className="text-white" /> : <HiOutlineMenu size={30} className="text-white" />}</button>
+//       </div>
+//       <div className={`md:flex ${menuOpen ? "flex" : "hidden"} w-full md:w-auto flex-col md:flex-row md:items-center`}>
+//         <NavbarContent justify="center" className="flex flex-col md:flex-row md:items-center">
+//           <NavLink href="/members" label="Matches" />
+//           <NavLink href="/lists" label="Lists" />
+//           <NavLink href="/messages" label="Messages" />
+//           <NavLink href="/news" label="News" />
+//         </NavbarContent>
+//         <NavbarContent justify="end" className="hidden md:flex flex-col md:flex-row md:items-center">
+//           <Button as={Link} href="/login" variant="bordered" className="text-white">
+//             Login
+//           </Button>
+//           <Button as={Link} href="/register" variant="bordered" className="text-white">
+//             Register
+//           </Button>
+//         </NavbarContent>
+//       </div>
+//       {/* Panel for mobile view */}
+//       {menuOpen && (
+//         <div className="absolute top-0 left-0 w-3/4 h-full bg-green-600 flex flex-col p-4 md:hidden">
+//           <NavLink href="/members" label="Matches" />
+//           <NavLink href="/lists" label="Lists" />
+//           <NavLink href="/messages" label="Messages" />
+//           <NavLink href="/news" label="News" />
+//           <Button as={Link} href="/login" variant="bordered" className="text-white mt-2">
+//             Login
+//           </Button>
+//           <Button as={Link} href="/register" variant="bordered" className="text-white mt-2">
+//             Register
+//           </Button>
+//         </div>
+//       )}
+//     </Navbar>
+//   )
+// }
+
 import { Button, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react"
 import Link from "next/link"
 import React from "react"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,66 @@
-import { nextui } from '@nextui-org/react';
-import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react"
+import type { Config } from "tailwindcss"
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  darkMode: 'class',
-  plugins: [nextui()],
-};
-export default config;
+  darkMode: "class",
+  plugins: [
+    nextui({
+      themes: {
+        "green-eck": {
+          extend: "light", // dark= vererbt Standardwerte vom dunklen Thema
+          colors: {
+            background: "#0D001A",
+            foreground: "#000000",
+            primary: {
+              50: "#F5EFE6",
+              100: "#EED9B7",
+              200: "#E6C48C",
+              300: "#D8A557",
+              400: "#C08F3A",
+              500: "#A47820",
+              600: "#846013",
+              700: "#63480B",
+              800: "#423106",
+              900: "#211803",
+              DEFAULT: "#A47820",
+              foreground: "#ffffff",
+            },
+            secondary: {
+              50: "#E6F4E9",
+              100: "#C1E1C8",
+              200: "#99D1A5",
+              300: "#68BB79",
+              400: "#45A358",
+              500: "#278C3A",
+              600: "#1D6E2E",
+              700: "#155122",
+              800: "#0D3415",
+              900: "#041808",
+              DEFAULT: "#278C3A",
+              foreground: "#ffffff",
+            },
+            focus: "#F182F6",
+          },
+          layout: {
+            disabledOpacity: "0.3",
+            radius: {
+              small: "4px",
+              medium: "6px",
+              large: "8px",
+            },
+            borderWidth: {
+              small: "1px",
+              medium: "2px",
+              large: "3px",
+            },
+          },
+        },
+      },
+    }),
+  ],
+}
+export default config
