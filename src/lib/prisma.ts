ba63f732fb357c09f85ae84834618248a6fb3a13
Eckhard Hagemeier <74151940+EckiHag@users.noreply.github.com>
2024-08-14 20:27:00 +0200

    env3

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,18 @@
 // prisma.ts
-
+const fs = require("fs")
 import "@/envConfig" // Läd die Umgebungsvariablen mit @next/env
 import { PrismaClient } from "@prisma/client"
 
 console.log("DATABASE_URL:", process.env.DATABASE_URL)
 
-export const prisma = new PrismaClient({ log: ["query"] })
+export const prisma = new PrismaClient({
+  log: ["query", "info", "warn", "error"],
+})
+
+const logStream = fs.createWriteStream("@/to/your/prisma.log", { flags: "a" })
+
+console.log = (msg) => logStream.write(msg + "\n")
+console.error = (msg) => logStream.write(msg + "\n")
 
 // const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
