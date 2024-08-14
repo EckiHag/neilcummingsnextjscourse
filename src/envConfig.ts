1ed86c75a989b677823581be9be10becc5ac9465
Eckhard Hagemeier <74151940+EckiHag@users.noreply.github.com>
2024-08-14 19:59:59 +0200

    env 2

--- src/envConfig.ts
+++ src/envConfig.ts
@@ -3,3 +3,4 @@ import { loadEnvConfig } from "@next/env"
 
 const projectDir = process.cwd()
 loadEnvConfig(projectDir)
+console.log("envConfig.ts loaded")
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -3,8 +3,12 @@
 import "@/envConfig" // Läd die Umgebungsvariablen mit @next/env
 import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+console.log("DATABASE_URL:", process.env.DATABASE_URL)
 
-export const prisma = globalForPrisma.prisma || new PrismaClient({ log: ["query"] })
+export const prisma = new PrismaClient({ log: ["query"] })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+// const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+// export const prisma = globalForPrisma.prisma || new PrismaClient({ log: ["query"] })
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
